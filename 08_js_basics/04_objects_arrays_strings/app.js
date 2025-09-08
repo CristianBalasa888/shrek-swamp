6d003b76f64182872bb4b02107c0b6bdae3a3f17
Cristian Balasa <cristian.balasa888@gmail.com>
2025-09-08 19:42:50 +0300

    Js basics: product cards

--- 08_js_basics/04_objects_arrays_strings/app.js
+++ 08_js_basics/04_objects_arrays_strings/app.js
@@ -1,12 +1,31 @@
 
-function calculeazaSuma() {
-var primulNumar = Number(document.getElementById
-('first-number').value);
-var alDoileaNumar = Number(document.getElementById
-('second-number').value);
+// function calculeazaSuma() {
+// var primulNumar = Number(document.getElementById
+// ('first-number').value);
+// var alDoileaNumar = Number(document.getElementById
+// ('second-number').value);
 
-console.log(primulNumar + alDoileaNumar)
-var suma = primulNumar + alDoileaNumar;
+// console.log(primulNumar + alDoileaNumar)
+// var suma = primulNumar + alDoileaNumar;
 
-document.getElementById('output').innerHTML= "Suma dintre " + primulNumar + " si " + alDoileaNumar + " este " + suma;
+// document.getElementById('output').innerHTML= "Suma dintre " + primulNumar + " si " + alDoileaNumar + " este " + suma;
+// }
+
+// var numbers = [6, 2, 7, 8, 9];
+
+var deCateOriSaDat66 = 0;
+var nrTotalDeIncercari = 0;
+while(true){
+    var zar1 = parseInt(Math.random() * 6) + 1;
+    var zar2 = parseInt(Math.random() * 6) + 1;
+    nrTotalDeIncercari++;
+    console.log(zar1 + " si " + zar2);  
+    if (zar1 === 6 && zar2 === 6){
+        deCateOriSaDat66=1000;
+    }
+    if (deCateOriSaDat66 === 1000){
+        console.log('Nr total de aruncari', nrTotalDeIncercari);
+        break;
+    }
+
 }
---
+++ 08_js_basics/08_show_products/app.js
@@ -0,0 +1,49 @@
+//  <div class="card">
+//             <p class="name">Lapte</p>
+//             <img src="/08_js_basics/08_show_products/lapte.jpg" alt="Product Image">
+//             <p class="price">10lei</p>
+//         </div>
+//          <div class="card">
+//             <p class="name">Mere</p>
+//             <img src="/08_js_basics/08_show_products/mere.jpg" alt="Product Image">
+//             <p class="price">17lei</p>
+//         </div>
+//          <div class="card">
+//             <p class="name">Rosii</p>
+//             <img src="/08_js_basics/08_show_products/rosii.jpg" alt="Product Image">
+//             <p class="price">5lei</p>
+//         </div>
+//         <div class="card">
+//             <p class="name">Paste</p>
+//             <img src="/08_js_basics/08_show_products/paste.jpg" alt="Product Image">
+//             <p class="price">8lei</p>
+//         </div>
+//         <div class="card">
+//             <p class="name">Cola</p>
+//             <img src="/08_js_basics/08_show_products/cocacola.jpg" alt="Product Image">
+//             <p class="price">6lei</p>
+//         </div>
+
+var products = [
+    {
+        name: "Lapte",
+        image: "/08_js_basics/08_show_products/lapte.jpg",
+        price: 10
+    },
+    {   name: "Mere",
+        image: "/08_js_basics/08_show_products/mere.jpg",
+        price: 17
+    },
+    {   name: "Rosii",
+        image: "/08_js_basics/08_show_products/rosii.jpg",
+        price: 5
+    },
+    {   name: "Paste",
+        image: "/08_js_basics/08_show_products/paste.jpg",
+        price: 8
+    },
+    {   name: "Cola",
+        image: "/08_js_basics/08_show_products/cocacola.jpg",
+        price: 6
+    }
+];
